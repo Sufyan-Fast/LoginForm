58709beebff2cd85febc24a2a5b33c00077e0e86
bilalashraf1710 <77960362+ashrafbilal862@users.noreply.github.com>
2022-09-21 16:33:08 +0500

    LoginFormUI

---
+++ force-app/main/default/lwc/loginForm/loginForm.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+import IMAGES from "@salesforce/resourceUrl/static_images";
+
+
+export default class LoginForm extends LightningElement {
+
+    iconImage= IMAGES + '/static_images/images/ProfileIcon.jpg';
+
+}
